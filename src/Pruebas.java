ae4555dd3ce00099a07f1826076d926a073bebcb
JoaquinHervasFarru <joaquinhervasfarru@gmail.com>
2018-03-09 10:40:04 -0500

    GetDay trial

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,9 +1,21 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 
 public class Pruebas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String dateInput = "09 01 1990";
+	
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yyyy");
+		LocalDate formatted = LocalDate.parse(dateInput, formatter);
 
+		DayOfWeek day = formatted.getDayOfWeek();
+
+		System.out.println(day);
+		
 	}
 
 }
